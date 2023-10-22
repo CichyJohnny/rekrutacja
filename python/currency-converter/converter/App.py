7084b38cc5e8667db3947aec28de7b81a36639ad
CichyJohnny <04cichy@gmail.com>
2023-10-23 01:03:18 +0200

    completed

--- python/currency-converter/converter/App.py
+++ python/currency-converter/converter/App.py
@@ -18,7 +18,7 @@ class App:
 
     def get_ratio(self):
         obtainer = RatioObtainer(base=self.base_currency, target=self.target_currency)
-        if not obtainer.was_ratio_saved_today():
-            return obtainer.fetch_ratio() #obtainer.get_matched_ratio_value()
+        if obtainer.was_ratio_saved_today():
+            return obtainer.get_matched_ratio_value()
         else:
             return obtainer.fetch_ratio()
--- python/currency-converter/converter/RatioObtainer.py
+++ python/currency-converter/converter/RatioObtainer.py
@@ -5,32 +5,29 @@ class RatioObtainer:
     base = None
     target = None
 
+
     def __init__(self, base, target):
         self.base = base
         self.target = target
+        self.data_file = '../ratios.json'
+        self.ratio_data = self.load_ratio_data()
+
+
+    def load_ratio_data(self):
+        if os.path.exists(self.data_file):
+            with open(self.data_file, 'r') as file:
+                data = json.load(file)
+
+            return data
+
+        return {}
+
 
     def was_ratio_saved_today(self):
-        if os.path.exists('../ratios.json'):
-            file = open('../ratios.json', 'r')
-            data = json.load(file)
-            file.close()
+        today = datetime.date.today().isoformat()
 
-            timestamp = data.get('timestamp')
-            humantime = datetime.datetime.fromtimestamp(timestamp)
+        return today in self.ratio_data and f"{self.base}{self.target}" in self.ratio_data[today]
 
-            if str(humantime).split()[0] == str(datetime.date.today()):
-
-                return True
-
-            else:
-
-                return False
-
-        else:
-            file = open('../ratios.json', 'a')
-            file.close()
-
-            return False
 
         # TODO
         # This function checks if given ratio was saved today and if the file with ratios is created at all
@@ -38,44 +35,52 @@ class RatioObtainer:
         # should return true otherwise
 
 
-        pass
-
     def fetch_ratio(self):
         base = str(self.base)
         target = str(self.target)
-        url_path = 'http://api.exchangerate.host/live?access_key=cd007cacde18395212e8ab232cb5f260&source={}&currencies={}&format=1' \
-            .format(base, target)
+        url_path = f'http://api.exchangerate.host/live?access_key=cd007cacde18395212e8ab232cb5f260&source={base}&format=1'
         response = requests.get(url_path)
-        self.save_ratio(response)
+        ratio_data = response.json()
+        self.save_ratio(ratio_data)
 
         quotes = response.json().get('quotes')
         ratio_value = quotes.get(base + target)
 
-        print("Quotes:", quotes)
-        print("Ratio Value:", ratio_value)
-
         return float(ratio_value)
+
 
         # TODO
         # This function calls API for today's exchange ratio
         # Should ask API for today's exchange ratio with given base and target currency
         # and call save_ratio method to save it
 
-    def save_ratio(self, ratio):
-        if self.was_ratio_saved_today():
-            pass
-        else:
-            pass
-        file = open('../ratios.json', 'w+')
-        file.write(json.dumps(ratio.json(), indent=4))
-        file.close()
+
+    def save_ratio(self, ratio_data):
+        today = datetime.date.today().isoformat()
+
+        if today not in self.ratio_data:
+            self.ratio_data[today] = {}
+        self.ratio_data[today][f"{self.base}{self.target}"] = ratio_data[f'quotes'][f"{self.base}{self.target}"]
+
+        with open(self.data_file, 'w') as file:
+            json.dump(self.ratio_data, file, indent=4)
+
 
         # TODO
         # Should save or update exchange rate for given pair in json file
         # takes ratio as argument
         # example file structure is shipped in project's directory, yours can differ (as long as it works)
 
+
     def get_matched_ratio_value(self):
+        today = datetime.date.today().isoformat()
+
+        if today in self.ratio_data and f"{self.base}{self.target}" in self.ratio_data[today]:
+            return float(self.ratio_data[today][f"{self.base}{self.target}"])
+
+        return None
+
+
+
         # TODO
         # Should read file and receive exchange rate for given base and target currency from that file
-        pass
