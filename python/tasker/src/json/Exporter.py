df9349f5ccfdef28ea0b1b824ad14950df2ce9ee
CichyJohnny <04cichy@gmail.com>
2023-10-24 22:12:26 +0200

    added save and load option to taski

--- python/tasker/src/json/Exporter.py
+++ python/tasker/src/json/Exporter.py
@@ -4,8 +4,16 @@ import json
 class Exporter:
 
     def __init__(self):
-        pass
+        self.data_file = 'taski.json'
 
     def save_tasks(self, tasks):
+
+
+
+        with open(self.data_file, 'w') as file:
+            updated_tasks = tasks
+
+
+            json.dump(updated_tasks, file, indent=4)
+
         # TODO zapisz taski do pliku tutaj
-        pass
--- python/tasker/src/json/Importer.py
+++ python/tasker/src/json/Importer.py
@@ -1,15 +1,29 @@
-import json
+import json, os
 
 
 class Importer:
 
     def __init__(self):
-        pass
+        self.data_file = 'taski.json'
+        self.taski = None
 
     def read_tasks(self):
+
+        current_directory = os.getcwd()
+        full_path = os.path.abspath(os.path.join(current_directory, self.data_file))
+
+        try:
+            with open(self.data_file, 'r') as file:
+                taski = json.load(file)
+        except FileNotFoundError:
+            print('File does not exist', full_path)
+        self.taski = taski
+
         # TODO odczytaj plik i zdekoduj treść tutaj
-        pass
 
     def get_tasks(self):
+
+        print('mmmm', self.taski)
+
+        return self.taski
         # TODO zwróć zdekodowane taski tutaj
-        pass
