e84d928f20cd8c219c41692cf75a0ab578755b80
CichyJohnny <04cichy@gmail.com>
2023-10-25 14:40:42 +0200

    word-rank problem solved and described

--- python/word-rank/script.py
+++ python/word-rank/script.py
@@ -1,5 +1,6 @@
-# coding=utf-8
+from time import perf_counter
 
+# coding=utf-8
 # input: array with multiple strings
 # expected output: rank of the 3 most often repeated words in given set of strings and number of times they occured, case insensitive
 
@@ -27,5 +28,33 @@ sentences = [
 # 2. "tak" - 5
 # 3. "z" - 2
 
-
 # Good luck! You can write all the code in this file.
+
+set_time = perf_counter()
+
+words = []
+for i in sentences:
+    one_word = i.replace(',', '').replace('.', '')
+    words.extend(one_word.lower().split())
+
+words.sort()
+
+# Firstly we get rid of all comas and periods
+# Then we create list of all words from sentences, lowered and splitted by space
+# Finally list is sorted alphabetically
+
+for i in range(3):
+
+    count = list(map(lambda x: words.count(x), words))
+    repetition = max(count)
+    max_word = words[count.index(repetition)]
+
+    words = list(filter(lambda x: x != max_word, words))
+
+    print(f'{i+1}.\t {max_word} \t - \t {repetition}')
+
+# In every loop we map a count list, as a list of repetition of every word in words list
+# Then we find first word (in alphabetical order) that appeared the most
+# Words is printed and removed from list, so in newly mapped count list the next max value can be found
+
+print(f'\nDone in {perf_counter() - set_time}s')
